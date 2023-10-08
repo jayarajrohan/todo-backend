d0aec09c47eadb2e440550ef9c4aaf2712b79935
jayarajrohan <iaminbest@gmail.com>
2023-10-08 12:06:45 +0530

    new environment variable added

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(compression());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_SERVER);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
